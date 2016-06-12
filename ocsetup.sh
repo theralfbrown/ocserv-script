1059e303fbb57a896cf84ef83f4e96200c8a435c
ralf <theralfbrown@users.noreply.github.com>
2016-06-12 14:12:13 +0200

    Update ocsetup.sh

--- ocsetup.sh
+++ ocsetup.sh
@@ -1,12 +1,9 @@
-# <------------------------------ Enviroment Variables ------------------------------->
-FQDN=ip of your server
-ORG_NAME=organization name or ip of server
-# <--------------------------------- Building --------------------------------------->
-
-rm /etc/apt/sources.list
+#!/bin/bash
 
 # ___________________________________FOR DEBIAN 8 (dont forget to comment debian 7) ____________________________________#
 
+function debian_8(){
+rm /etc/apt/sources.list
 cat << _EOF_ > /etc/apt/sources.list
 deb http://httpredir.debian.org/debian jessie main
 deb-src http://httpredir.debian.org/debian jessie main
@@ -16,49 +13,54 @@ deb-src http://httpredir.debian.org/debian jessie-updates main
 
 deb http://security.debian.org/ jessie/updates main
 deb-src http://security.debian.org/ jessie/updates main
-#backport
+# backport
 deb http://ftp.debian.org/debian jessie-backports main contrib non-free
 _EOF_
-
+}
 
 # ___________________________________FOR DEBIAN 7 (dont forget to comment debian 8)____________________________________#
 
-# cat << _EOF_ > /etc/apt/sources.list
-#deb http://ftp.debian.org/debian stable main contrib non-free
-#eb-src http://ftp.debian.org/debian stable main contrib non-free
+function debian_7(){
+rm /etc/apt/sources.list
+cat << _EOF_ > /etc/apt/sources.list
+deb http://ftp.debian.org/debian stable main contrib non-free
+deb-src http://ftp.debian.org/debian stable main contrib non-free
 
-#deb http://ftp.debian.org/debian/ wheezy-updates main contrib non-free
-#deb-src http://ftp.debian.org/debian/ wheezy-updates main contrib non-free
+deb http://ftp.debian.org/debian/ wheezy-updates main contrib non-free
+deb-src http://ftp.debian.org/debian/ wheezy-updates main contrib non-free
 
-#deb http://security.debian.org/ wheezy/updates main contrib non-free
-#deb-src http://security.debian.org/ wheezy/updates main contrib non-free
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb-src http://security.debian.org/ wheezy/updates main contrib non-free
+
 # #backport
-# deb http://ftp.debian.org/debian wheezy-backports main contrib non-free
-# _EOF_
+deb http://ftp.debian.org/debian wheezy-backports main contrib non-free
+_EOF_
+}
 
 
-#echo "deb http://ftp.debian.org/debian jessie-backports main contrib non-free" | tee -a /etc/apt/sources.list
-
+# ------------------ GET AND INSTALL----------------------#
+function get_essential(){
 apt-get update
 apt-get install build-essential
-apt-get install libgnutls28-dev libwrap0-dev libpam0g-dev libseccomp-dev libnl-route-3-dev
-apt-get install libgmp3-dev m4 gcc pkg-config make gnutls-bin libreadline-dev
+apt-get install libgnutls28-dev libwrap0-dev libpam0g-dev libseccomp-dev libnl-route-3-dev libev4 libev-dev
+apt-get install libgmp3-dev m4 gcc pkg-config make gnutls-bin libreadline-dev curl libprotobuf-c0-dev protobuf-c-compiler
+}
 
 # Get OCServ
-wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.10.9.tar.xz
-tar xvf ocserv-0.10.9.tar.xz
-cd ocserv-0.10.9
+function get_oc_install(){
+oc_version_latest=$(curl -s "http://www.infradead.org/ocserv/download.html" | sed -n 's/^.*version is <b>\(.*$\)/\1/p')
+wget ftp://ftp.infradead.org/pub/ocserv/ocserv-"$oc_version_latest".tar.xz
+tar xvf ocserv-"$oc_version_latest".tar.xz
+cd ocserv-"$oc_version_latest"
 ./configure
 make
 make install
+}
 
-echo " ***************  make complete ***************"
-
-
-# <------------------------------ Keypair Generation ------------------------------->
-
+function certificate(){
+FQDN=$(wget -qO- ipv4.icanhazip.com)
+ORG_NAME=$FQDN
 certtool --generate-privkey --outfile ca-key.pem 
-
 echo "cn = $FQDN" > ca.tmpl
 echo "organization = $ORG_NAME" >> ca.tmpl
 echo "serial = 1" >> ca.tmpl
@@ -67,20 +69,13 @@ echo "ca" >> ca.tmpl
 echo "signing_key" >> ca.tmpl
 echo "cert_signing_key" >> ca.tmpl
 echo "crl_signing_key">> ca.tmpl
-
-echo "*************** ca-key.pem & ca.tmpl ******************"
-
 certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca-cert.pem
-
 echo "cn = $FQDN" > server.tmpl
 echo "organization = $ORG_NAME" >> server.tmpl
 echo "expiration_days = 3650" >> server.tmpl
 echo "signing_key" >> server.tmpl
 echo "encryption_key" >> server.tmpl
 echo "tls_www_server" >> server.tmpl
-
-echo "********************ca-cert.pem & server.tmpl *******************"
-
 certtool --generate-privkey --outfile server-key.pem
 echo "**********  server-key.pem**************"
 certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
@@ -88,121 +83,154 @@ echo "*******************server-cert.pem *******************"
 echo "crl_next_update = 999" >crl.tmpl
 echo "crl_number = 1">>crl.tmpl
 echo "************ crl.tmpl **************"
-
-mkdir /etc/ocserv
+mkdir /etc/ocserv/
 mkdir /etc/ocserv/ssl
 cp server-cert.pem /etc/ocserv/ssl/
 cp server-key.pem /etc/ocserv/ssl/
+}
 
-echo "************ copy cert compelete & make dir**************"
 
-
+#-----------------auto conf making ---------------------------#
+function conf_creation(){
 cat << _EOF_ > /etc/ocserv/ocserv.conf
-
 auth = "plain[/etc/ocserv/ocpasswd]"
-
-
-# TCP and UDP port number
-tcp-port = 443
-udp-port = 443
-
-# The user the worker processes will be run as. It should be
-# unique (no other services run as this user).
+tcp-port = 999
+udp-port = 999
 run-as-user = nobody
 run-as-group = daemon
-
-# socket file used for server IPC (worker-main), will be appended with .PID
-# It must be accessible within the chroot environment (if any), so it is best
-# specified relatively to the chroot directory.
 socket-file = /var/run/ocserv-socket
-
 server-cert = /etc/ocserv/ssl/server-cert.pem
 server-key = /etc/ocserv/ssl/server-key.pem
-#ca-cert = ../tests/ca.pem
-
-# The performance cost is roughly 2% overhead at transfer time (tested on a Linux 3.17.8).
 isolate-workers = true
-
-# Limit the number of clients. Unset or set to zero for unlimited.
-#max-clients = 1024
 max-clients = 16
-
-# Limit the number of identical clients (i.e., users connecting 
-# multiple times). Unset or set to zero for unlimited.
-max-same-clients = 2
-
-# Keepalive in seconds
-keepalive = 32400
-
-
-dpd = 90
-
-mobile-dpd = 1800
-
-# MTU discovery (DPD must be enabled)
+max-same-clients = 1
+keepalive = 300
+dpd = 10
 try-mtu-discovery = true
-
-# The object identifier that will be used to read the user ID in the client 
-# certificate. The object identifier should be part of the certificate's DN
-# Useful OIDs are: 
-#  CN = 2.5.4.3, UID = 0.9.2342.19200300.100.1.1
 cert-user-oid = 0.9.2342.19200300.100.1.1
-
 tls-priorities = "NORMAL:%SERVER_PRECEDENCE:%COMPAT:-VERS-SSL3.0"
-
 auth-timeout = 40
-
 # Set to zero to disable.
 max-ban-score = 50
-
 # The time (in seconds) that all score kept for a client is reset.
 ban-reset-time = 300
-
 cookie-timeout = 300
-
 deny-roaming = false
-
 rekey-time = 172800
-
 rekey-method = ssl
-
 use-occtl = true
-
 pid-file = /var/run/ocserv.pid
-
 device = vpns
-
 predictable-ips = true
-
 default-domain = example.com
-
 ipv4-network = 10.10.10.0
 ipv4-netmask = 255.255.255.0
-
 dns = 8.8.8.8
-#dns = 4.2.2.4
-
+dns = 4.2.2.4
 ping-leases = false
-
-#route = 10.10.10.0/255.255.255.0
-#route = 192.168.0.0/255.255.0.0
-#route = fef4:db8:1000:1001::/64
-# Subsets of the routes above that will not be routed by
-# the server.
-#no-route = 192.168.5.0/255.255.255.0
-
 cisco-client-compat = true
-
 _EOF_
+}
+
+function iptable_rules(){
+sysctl -w net.ipv4.ip_forward=1
+sysctl -p
+iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE
+iptables -A FORWARD -s 10.10.10.0/24 -j ACCEPT
+iptables -I INPUT -p tcp --dport 999 -j ACCEPT
+iptables -I INPUT -p udp --dport 999 -j ACCEPT
+}
 
 
-#confile
-echo "***************** conf file maked *************************"
+function user_add(){
+read -p "Enter username : " username
+read -p "Enter password : " password
+echo $password | ocpasswd -c /etc/ocserv/ocpasswd $username
+}
+
+function title(){
+clear
+echo "##############################_______OC SERVE V2 ________###################################"
+}
+
+function help_(){
+echo "USAGE :"
+echo "ocsetup.sh {(-i)nstall|start|stop|(-s)tatus|(-h)elp}"
+}
+
+function start(){
+iptable_rules
+ocserv -c /etc/ocserv/ocserv.conf
+}
 
 
-sysctl -w net.ipv4.ip_forward=1 && sysctl -p && iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu && iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE && iptables -A FORWARD -s 10.10.10.0/24 -j ACCEPT && iptables -I INPUT -p tcp --dport 443 -j ACCEPT && iptables -I INPUT -p udp --dport 443 -j ACCEPT
+function stop(){
+/etc/init.d/ocserv stop
+pkill ocserv
+iptables -F
+echo "OCserv STOPED"
+}
 
-echo "	now create login user as blew :"
-echo "	(ocpasswd -c /etc/ocserv/ocpasswd <YOURDESIREDUSERNAME>)"
-echo " ************ Every things are OK enjoy ocserv **************"
-echo "	to start : ocserv -c /etc/ocserv/ocserv.conf"
+function status(){
+if [[ -f /run/ocserv.pid ]]; then
+echo "OCserv in running"
+elif [[ ! -f /run/ocserv.pid ]]; then
+echo "OCserv in NOT running"
+fi
+}
+
+function get_os_info(){
+read -p "Enter Debian root version ( 7 or 8 ) : " os_version
+}
+
+
+#initsh
+action=$1
+[  -z $1 ] && action=help
+case "$action" in
+install | -i)
+clear
+title
+get_os_info
+if [[ $os_version="7" ]]; then
+	debian_7
+elif [[ $os_version="8" ]]; then
+	debian_8
+fi
+get_essential
+get_oc_install
+certificate
+clear
+conf_creation
+user_add
+clear
+title
+echo "OCServ install Successfully"
+    ;;
+stop)
+clear
+title
+stop
+    ;;
+start)
+start
+clear
+title
+echo "OCserv NOW STARTED"
+    ;;
+status | -s)
+clear
+title
+status
+    ;;
+help | -h)
+clear
+help_
+    ;;
+*)
+clear
+help_
+    ;;
+esac
+exit 0
